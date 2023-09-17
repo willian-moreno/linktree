ca1324e7723bef0020d2afc9c09b37130b8f1a5e
willian-moreno <willian_moreno@outlook.com>
2023-09-17 17:06:33 -0300

    feat: Bubbles animation

--- assets/js/script.js
+++ assets/js/script.js
@@ -71,3 +71,50 @@ function isSystemThemeDark() {
 function handleFooterCurrentYear() {
 	document.body.querySelector(".wrapper-footer .footer-year").innerHTML = new Date().getFullYear()
 }
+
+const observer = new MutationObserver(function (mutations) {
+	mutations.forEach(function (mutation) {
+		const { type, attributeName, target } = mutation
+		if (!["attributes", "attributeFilter"].includes(type) || attributeName !== "class") return
+
+		if (target.classList.contains("show")) {
+			top.interval = setInterval(createBubble, 1000)
+			return
+		}
+
+		clearInterval(top.interval)
+	})
+})
+
+const bubblesWrapper = document.querySelector(".bubbles-wrapper")
+observer.observe(bubblesWrapper, { attributes: true, attributeFilter: ["class"] })
+
+function createBubble() {
+	const bubblesWrapper = document.querySelector(".bubbles-wrapper")
+	const { width } = bubblesWrapper.getBoundingClientRect()
+
+	const bubbleLeft = Math.floor(Math.random() * (width - 1)) + 1
+	const bubbleBlur = Math.floor(Math.random() * 10) + 2
+	const bubbleWidth = Math.floor(Math.random() * 10) + 5
+
+	const bubble = document.createElement("span")
+	bubble.classList.add("bubble")
+	bubble.style.left = bubbleLeft + "px"
+	bubble.style.filter = `blur(${bubbleBlur}px)`
+	bubble.style.width = bubbleWidth + "px"
+
+	bubblesWrapper.appendChild(bubble)
+
+	const duration = getComputedStyle(bubble).animationDuration
+	const durationInSecondsRegex = new RegExp(/^\d+s$/)
+	let durationToMilliseconds = duration.replaceAll(/\D/g, "")
+	if (durationInSecondsRegex.test(duration)) {
+		durationToMilliseconds = durationToMilliseconds * 1000
+	}
+
+	setTimeout(() => {
+		bubble.remove()
+	}, durationToMilliseconds)
+}
+
+top.interval = setInterval(createBubble, 500)
